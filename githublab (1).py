f53d8ecbacf757318a862feb033c903656e7fa7c
marronangel <marron.angel@csmstudents.org>
2019-05-16 16:26:59 +0000

    luis

--- githublab (1).py
+++ githublab (1).py
@@ -2,4 +2,6 @@ x=input('Hello World')
 if(x=='Hello' or x=='hello'):
     print('Nice to meet you')
 elif(x=='Hi'or x=='hi'):
-    print('Nice to meet you')    
+    print('Nice to meet you')
+print('1')
+print('2')
